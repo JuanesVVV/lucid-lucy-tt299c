663200cd90c9db024ad14e0bd5e29c263231309c
Juan Esteban <102327436+JuanesVVV@users.noreply.github.com>
2025-08-19 20:34:59 -0500

    Initial commit

---
+++ src/SobreMi/Informacion.js
@@ -0,0 +1,31 @@
+function Datos() {
+  return (
+    <div>
+      <h2>Sobre Mi</h2>
+      <p>
+        <strong>Mi nombre:</strong> Juan Esteban Rodriguez Ramos
+      </p>
+      <p>
+        <strong>Mi meta:</strong> Aprender bastante en la vida
+      </p>
+      <p>
+        <strong>Mi comida favorita:</strong> Pizza
+      </p>
+      <p>
+        <strong>Mi color favorito:</strong> Azul
+      </p>
+      <p>
+        <strong>Mi edad:</strong> 18 años
+      </p>
+      <p>
+        <strong>Mis pasatiempos:</strong> Jugar videoluegos y leer
+      </p>
+      <p>
+        <strong>Mi descripcion personal:</strong> Diria que soy un chico muy
+        cerrado e intento conversar, intento ser carismatico, intento ayudar a
+        la gente y busco crecer como persona
+      </p>
+    </div>
+  );
+}
+export default Datos;
